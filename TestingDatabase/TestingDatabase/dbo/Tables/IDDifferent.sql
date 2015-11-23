2564903016624f32206df3fea18490ceed135ec1
kacollins <kimberlyacollins@gmail.com>
2015-11-22 18:35:27 -0600

    Added tables for testing data comparison scenarios.

---
+++ TestingDatabase/TestingDatabase/dbo/Tables/IDDifferent.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].IDDifferent (
+    [ID]           INT UNIQUE    NOT NULL,
+    Value          VARCHAR (50)  NOT NULL,
+    [DateCreated]  DATETIME      NOT NULL,
+    [UserCreated]  VARCHAR (100) NOT NULL,
+    [DateModified] DATETIME      NOT NULL,
+    [UserModified] VARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
