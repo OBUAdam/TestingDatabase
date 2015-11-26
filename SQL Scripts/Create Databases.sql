35566ca78b3d7e1b5dac67e5ede38878a02a377b
OBUAdam <AdamBCollins@gmail.com>
2015-11-26 10:47:00 -0600

    Got rid of SQL scripts.

--- SQL Scripts/Create Databases.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE Database1 ON (name = 'Database1', filename = 'C:\Git Repos\TestingDatabase\Databases\Database1.mdf');
-GO
-CREATE DATABASE Database2 ON (name = 'Database2', filename = 'C:\Git Repos\TestingDatabase\Databases\Database2.mdf');
-GO
